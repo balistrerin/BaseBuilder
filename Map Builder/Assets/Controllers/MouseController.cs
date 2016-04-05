45b8bc9002cd38402c4f91e8a85599b49b407f25
Nick Balistreri <balistrerin@gmail.com>
2016-04-05 10:22:51 -0700

    Add Drag Floor builder.

--- Map Builder/Assets/Controllers/MouseController.cs
+++ Map Builder/Assets/Controllers/MouseController.cs
@@ -6,6 +6,7 @@ public class MouseController : MonoBehaviour {
 	public GameObject circleCursor;
 
 	Vector3 lastFramePosition;
+	Vector3 dragStartPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +18,49 @@ public class MouseController : MonoBehaviour {
 		Vector3 currFramePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		currFramePosition.z = 0;
 
-		circleCursor.transform.position = currFramePosition;
+		// Update the circle cursor position
+		Tile tileUnderMouse = GetTileAtWorldCoord (currFramePosition);
+		if (tileUnderMouse != null) {
+			circleCursor.SetActive (true);
+			Vector3 cursorPosition = new Vector3 (tileUnderMouse.X, tileUnderMouse.Y, 0);
+			circleCursor.transform.position = cursorPosition;
+		} else {
+			circleCursor.SetActive (false);
+		}
+
+		// Start Drag
+		if (Input.GetMouseButtonDown (0)) {
+
+			dragStartPosition = currFramePosition;
+		}
+
+		// End Drag
+		if (Input.GetMouseButtonUp (0)) {
+			int start_x = Mathf.FloorToInt (dragStartPosition.x);
+			int end_x = Mathf.FloorToInt (currFramePosition.x);
+			if (end_x < start_x) {
+				int temp = end_x;
+				end_x = start_x;
+				start_x = temp;
+			}
+
+			int start_y = Mathf.FloorToInt (dragStartPosition.y);
+			int end_y = Mathf.FloorToInt (currFramePosition.y);
+			if (end_y < start_y) {
+				int temp = end_y;
+				end_y = start_y;
+				start_y = temp;
+			}
+
+			for (int x = start_x; x <= end_x; x++) {
+				for (int y = start_y; y <= end_y; y++) {
+					Tile t = WorldController.Instance.World.GetTileAt (x, y);
+					if (t != null) {
+						t.Type = Tile.TileType.Floor;
+					}
+				}
+			}
+		}
 
 		if (Input.GetMouseButton (1) || Input.GetMouseButton (2)) {
 
@@ -27,5 +70,14 @@ public class MouseController : MonoBehaviour {
 
 		lastFramePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		lastFramePosition.z = 0;
+	}
+
+	Tile GetTileAtWorldCoord(Vector3 coord){
+
+		int x = Mathf.FloorToInt (coord.x);
+		int y = Mathf.FloorToInt (coord.y);
+
+		return WorldController.Instance.World.GetTileAt (x, y);
+
 	}
 }
--- Map Builder/Assets/Controllers/WorldController.cs
+++ Map Builder/Assets/Controllers/WorldController.cs
@@ -2,24 +2,34 @@
 using System.Collections;
 
 public class WorldController : MonoBehaviour {
-	
+
+
+	public static WorldController Instance{ get; protected set;}
+
 	public Sprite floorSprite;
 
-	World world;
+	public World World { get; protected set;}
 
 	// Use this for initialization
 	void Start () {
-		world = new World();
+		if (Instance != null) {
+			Debug.LogError("There should nver be two world controllers.");
+		}
+
+		Instance = this;
+
+		World = new World();
 
 
 		//Create GameObject for each of our tiles, so they show visually.
-		for (int x = 0; x < world.Width; x++) {
-			for (int y = 0; y < world.Height; y++) {
-				Tile tile_data = world.GetTileAt (x, y);
+		for (int x = 0; x < World.Width; x++) {
+			for (int y = 0; y < World.Height; y++) {
+				Tile tile_data = World.GetTileAt (x, y);
 
 				GameObject tile_go = new GameObject ();
 				tile_go.name = "Tile_" + x + "_" + y;
 				tile_go.transform.position = new Vector3 (tile_data.X, tile_data.Y, 0);
+				tile_go.transform.SetParent (this.transform, true);
 
 				tile_go.AddComponent<SpriteRenderer> ();
 
@@ -27,7 +37,7 @@ public class WorldController : MonoBehaviour {
 
 			}
 		}
-		world.RandomizeTiles();
+		World.RandomizeTiles();
 	}
 	void Update(){
 		
