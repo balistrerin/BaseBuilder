decbd8f3b1f306465130cd32dfa5ffc66ec01cfe
Nick Balistreri <balistrerin@gmail.com>
2016-04-05 00:12:18 -0700

    Added cursor and camera movement

---
+++ Map Builder/Assets/Controllers/MouseController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	public GameObject circleCursor;
+
+	Vector3 lastFramePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currFramePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		currFramePosition.z = 0;
+
+		circleCursor.transform.position = currFramePosition;
+
+		if (Input.GetMouseButton (1) || Input.GetMouseButton (2)) {
+
+			Vector3 diff = lastFramePosition - currFramePosition;
+			Camera.main.transform.Translate (diff);
+		}
+
+		lastFramePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		lastFramePosition.z = 0;
+	}
+}
